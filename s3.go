b32375a24fb48b5d9f647cdc380c6b01a8a916be
alex <uohzxela@gmail.com>
2015-05-13 14:48:05 +0800

    implement ticker and max_buffer_size features

--- s3.go
+++ s3.go
@@ -3,6 +3,8 @@ package s3
 import (
 	"fmt"
 	"errors"
+	"bytes"
+	"time"
 	"github.com/mozilla-services/heka/message"
 	. "github.com/mozilla-services/heka/pipeline"
     "github.com/mitchellh/goamz/aws"
@@ -15,6 +17,8 @@ type S3OutputConfig struct {
 	Region string `toml:"region"`
 	BucketName string `toml:"bucket_name"`
 	PathName string `toml:"path_name"`
+	TickerInterval uint `toml:"ticker_interval"`
+	MaxBufferSize uint32 `toml:"max_buffer_size"`
 
 }
 
@@ -22,6 +26,7 @@ type S3Output struct {
 	config *S3OutputConfig
 	client *s3.S3
 	bucket *s3.Bucket
+	stopChan chan bool
 }
 
 func (so *S3Output) ConfigStruct() interface{} {
@@ -46,44 +51,60 @@ func (so *S3Output) Init(config interface{}) (err error) {
 
 func (so *S3Output) Run(or OutputRunner, h PluginHelper) (err error) {
 	inChan := or.InChan()
+	tickerChan := or.Ticker()
+	so.stopChan = make(chan bool)
+	buf := make([]byte, so.MaxBufferSize * 1024)
+	buffer := bytes.NewBuffer(buf)
 
 	var pack *PipelinePack
 	var msg *message.Message
 
-	for pack = range inChan {
-		msg = pack.Message
-		err = so.bucket.Put(so.config.PathName, []byte(msg.GetPayload()), "text/plain", "public-read")
-		if err != nil {
-			or.LogMessage(fmt.Sprintf("warning, unable to parse payload: %s", err))
-			err = nil
+	ok := true
+
+	for ok {
+		select {
+		case _, ok = <- so.stopChan:
 			continue
+		case <- tickerChan:
+			for pack = range inChan {
+				msg = pack.Message
+				n, err := buffer.Write([]byte(msg.GetPayload())
+				if err != nil {
+					err := so.Upload(buffer)
+					if err != nil {
+						or.LogMessage(fmt.Sprintf("warning, unable to upload payload when buffer is full: %s", err))
+						err = nil
+						continue
+					}
+					pack.Recycle()
+					break
+				}
+				pack.Recycle()
+			}
+			err := so.Upload(buffer)
+			if err != nil {
+				or.LogMessage(fmt.Sprintf("warning, unable to upload payload after ticker: %s", err))
+				err = nil
+				continue
+			}
 		}
-		pack.Recycle()
 	}
+
 	or.LogMessage("shutting down AWS S3 output runner")
 	return
 }
 
-// func (so *KafkaOutput) CleanupForRestart() {
-// 	so.client.Close()
-// 	ao.producer.Close()
-// 	ao.init()
-// }
+func (so *S3Output) Stop() {
+	close(so.stopChan)
+}
 
-// init aws client
-// func (ao *KafkaOutput) init() (err error) {
-// 	cconf := sarama.NewClientConfig()
-// 	ao.client, err = sarama.NewClient(ao.config.Id, ao.addrs, cconf)
-// 	if err != nil {
-// 		return
-// 	}
-// 	kconf := sarama.NewProducerConfig()
-// 	ao.producer, err = sarama.NewProducer(ao.client, kconf)
-// 	if err != nil {
-// 		return
-// 	}
-// 	return
-// }
+func (so *S3Output) Upload(buffer *Buffer) (err error) {
+	t := time.Now().Local().Format("20060102150405")
+	path := so.config.PathName + "/" + t 
+	err = so.bucket.Put(path, buffer.Bytes(), "text/plain", "public-read")
+	buffer.Reset()
+	return
+}
 
 func init() {
 	RegisterPlugin("S3Output", func() interface{} {
