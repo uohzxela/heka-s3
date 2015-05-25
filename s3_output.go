f9832e254e3a11fab25da521d29e462a9aed21f7
alex <uohzxela@gmail.com>
2015-05-25 19:05:55 +0800

    flesh out buffer-to-disk functions

--- s3_output.go
+++ s3_output.go
@@ -5,6 +5,9 @@ import (
 	"errors"
 	"bytes"
 	"time"
+	"os"
+	"strings"
+	"ioutil"
 	"compress/gzip"
 	"github.com/mozilla-services/heka/message"
 	. "github.com/mozilla-services/heka/pipeline"
@@ -20,12 +23,14 @@ type S3OutputConfig struct {
 	Prefix string `toml:"prefix"`
 	TickerInterval uint `toml:"ticker_interval"`
 	Compression bool `toml:"compression"`
+	BufferPath string  `toml:"buffer_path"`
 }
 
 type S3Output struct {
 	config *S3OutputConfig
 	client *s3.S3
 	bucket *s3.Bucket
+	bufferFilePath string
 }
 
 func (so *S3Output) ConfigStruct() interface{} {
@@ -45,6 +50,10 @@ func (so *S3Output) Init(config interface{}) (err error) {
 	}
 	so.client = s3.New(auth, region)
 	so.bucket = so.client.Bucket(so.config.Bucket)
+
+	prefixList := strings.Split(so.config.Prefix, "/")
+	bufferFileName = so.config.Bucket + "_" + strings.Join(prefixList, "_")
+	so.bufferFilePath = so.config.BufferPath + "/" + bufferFileName
 	return
 }
 
@@ -65,8 +74,8 @@ func (so *S3Output) Run(or OutputRunner, h PluginHelper) (err error) {
 			if !ok {
 				break
 			}
-			msg = pack.Message
-			_, err := buffer.Write([]byte(msg.GetPayload()))
+			msg = pack.Message\
+			err := so.WriteToBuffer(buffer, msg)
 			if err != nil {
 				or.LogMessage(fmt.Sprintf("Warning, unable to write to buffer: %s", err))
 				err = nil
@@ -90,7 +99,60 @@ func (so *S3Output) Run(or OutputRunner, h PluginHelper) (err error) {
 	return
 }
 
+func (so *S3Output) WriteToBuffer(buffer *bytes.Buffer, msg *message.Message) (err error) {
+	_, err := buffer.Write([]byte(msg.GetPayload()))
+	if err != nil {
+		return
+	}
+	if buffer.Len() > so.config.BufferChunkLimit {
+		err := so.SaveToDisk(buffer)
+	}
+	return
+}
+
+func (so *S3Output) SaveToDisk(buffer *bytes.Buffer) (err error) {
+	// TODO: save buffer to disk using buffer path (so.config.BufferPath)
+	// check if buffer/path/file exists, if not create it first
+	// append string to file
+
+	// if ok, err := exists(so.config.BufferPath); !ok {
+	// 	os.Mkdir(so.config.BufferPath, 0644)
+	// }
+
+	if ok, err := exists(so.bufferFilePath); !ok {
+		w, err := os.Create(so.bufferFilePath)
+		w.Close()
+	}
+	if err != nil {
+		return
+	}
+	if f, err := os.OpenFile(so.bufferFilePath, os.O_APPEND|os.O_WRONLY, 0666); err != nil {
+		return
+	}
+	if _, err = f.Write(buffer.Bytes()); err != nil {
+	    return
+	}
+	f.Close()
+
+	buffer.Reset()
+	return
+}
+
+func (so *S3Output) ReadFromDisk() (buffer *bytes.Buffer, err error) {
+	buf, err := ioutil.ReadFile(so.bufferFilePath)
+	buffer := bytes.NewBuffer(buf)
+
+	return buffer, err
+}
+
 func (so *S3Output) Upload(buffer *bytes.Buffer) (err error) {
+	if err := so.SaveToDisk(buffer); err != nil {
+		return
+	}
+	if buffer, err := so.ReadFromDisk(); err != nil {
+		return
+	}
+
 	if buffer.Len() == 0 {
 		err = errors.New("Buffer is empty.")
 		return
@@ -106,13 +168,24 @@ func (so *S3Output) Upload(buffer *bytes.Buffer) (err error) {
 		writer.Close()
 
 		path := so.config.Prefix + "/" + currentDate + "/" + currentTime + ".gz"
-		err = so.bucket.Put(path, buf.Bytes(), "application/zip", "public-read")
+		err = so.bucket.Put(path, buf.Bytes(), "multipart/x-gzip", "public-read")
 	} else {
 		path := so.config.Prefix + "/" + currentDate + "/" + currentTime 
 		err = so.bucket.Put(path, buffer.Bytes(), "text/plain", "public-read")
 	}
+	// TODO: if err is nil, remove buffer file on disk after upload
+	if err == nil {
+		err := os.Remove(so.bufferFilePath)
+	}
 
 	return
+}
+
+func exists(path string) (bool, error) {
+    _, err := os.Stat(path)
+    if err == nil { return true, nil }
+    if os.IsNotExist(err) { return false, nil }
+    return false, err
 }
 
 func init() {
