a07f3e2e55587b0a2b00d6cd308308903aa5ce7c
alex <uohzxela@gmail.com>
2015-05-26 10:57:48 +0800

    resolve compilation issues

--- s3_output.go
+++ s3_output.go
@@ -24,6 +24,8 @@ type S3OutputConfig struct {
 	TickerInterval uint `toml:"ticker_interval"`
 	Compression bool `toml:"compression"`
 	BufferPath string  `toml:"buffer_path"`
+	BufferChunkLimit int  `toml:"buffer_chunk_limit"`
+	
 }
 
 type S3Output struct {
@@ -52,7 +54,7 @@ func (so *S3Output) Init(config interface{}) (err error) {
 	so.bucket = so.client.Bucket(so.config.Bucket)
 
 	prefixList := strings.Split(so.config.Prefix, "/")
-	bufferFileName = so.config.Bucket + "_" + strings.Join(prefixList, "_")
+	bufferFileName := so.config.Bucket + "_" + strings.Join(prefixList, "_")
 	so.bufferFilePath = so.config.BufferPath + "/" + bufferFileName
 	return
 }
@@ -74,7 +76,7 @@ func (so *S3Output) Run(or OutputRunner, h PluginHelper) (err error) {
 			if !ok {
 				break
 			}
-			msg = pack.Message\
+			msg = pack.Message
 			err := so.WriteToBuffer(buffer, msg)
 			if err != nil {
 				or.LogMessage(fmt.Sprintf("Warning, unable to write to buffer: %s", err))
@@ -100,12 +102,12 @@ func (so *S3Output) Run(or OutputRunner, h PluginHelper) (err error) {
 }
 
 func (so *S3Output) WriteToBuffer(buffer *bytes.Buffer, msg *message.Message) (err error) {
-	_, err := buffer.Write([]byte(msg.GetPayload()))
+	_, err = buffer.Write([]byte(msg.GetPayload()))
 	if err != nil {
 		return
 	}
 	if buffer.Len() > so.config.BufferChunkLimit {
-		err := so.SaveToDisk(buffer)
+		err = so.SaveToDisk(buffer)
 	}
 	return
 }
@@ -118,16 +120,18 @@ func (so *S3Output) SaveToDisk(buffer *bytes.Buffer) (err error) {
 	// if ok, err := exists(so.config.BufferPath); !ok {
 	// 	os.Mkdir(so.config.BufferPath, 0644)
 	// }
-
-	if ok, err := exists(so.bufferFilePath); !ok {
-		w, err := os.Create(so.bufferFilePath)
+	
+	ok, err := exists(so.bufferFilePath)
+	if !ok {
+		w, _ := os.Create(so.bufferFilePath)
 		w.Close()
 	}
 	if err != nil {
 		return
 	}
-	if f, err := os.OpenFile(so.bufferFilePath, os.O_APPEND|os.O_WRONLY, 0666); err != nil {
-		return
+	f, err := os.OpenFile(so.bufferFilePath, os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
 	}
 	if _, err = f.Write(buffer.Bytes()); err != nil {
 	    return
@@ -140,17 +144,17 @@ func (so *S3Output) SaveToDisk(buffer *bytes.Buffer) (err error) {
 
 func (so *S3Output) ReadFromDisk() (buffer *bytes.Buffer, err error) {
 	buf, err := ioutil.ReadFile(so.bufferFilePath)
-	buffer := bytes.NewBuffer(buf)
+	buffer = bytes.NewBuffer(buf)
 
 	return buffer, err
 }
 
 func (so *S3Output) Upload(buffer *bytes.Buffer) (err error) {
 	if err := so.SaveToDisk(buffer); err != nil {
-		return
+		return err
 	}
-	if buffer, err := so.ReadFromDisk(); err != nil {
-		return
+	if buffer, err = so.ReadFromDisk(); err != nil {
+		return err
 	}
 
 	if buffer.Len() == 0 {
@@ -175,7 +179,7 @@ func (so *S3Output) Upload(buffer *bytes.Buffer) (err error) {
 	}
 	// TODO: if err is nil, remove buffer file on disk after upload
 	if err == nil {
-		err := os.Remove(so.bufferFilePath)
+		err = os.Remove(so.bufferFilePath)
 	}
 
 	return
